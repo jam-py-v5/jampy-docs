3efaa610d391b26d7f441572c8d92f43e0b85a71
platipusica <babicdd@gmail.com>
2026-04-10 08:37:33 +0000

    rm templt

--- conf.py
+++ conf.py
@@ -67,7 +67,7 @@ html_baseurl = 'https://jampyapplicationbuilder.com/docs/'
 
 # Add any paths that contain templates here, relative to this directory.
 
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 
 
